37f3fa30ebf6fc8ffb6a333a4fee6426258bbd0a
daxaker <itjustsoap@gmail.com>
2022-03-21 20:10:07 +0300

    add wrapper files

---
+++ src/Blend2DNet.cpp
@@ -0,0 +1,25 @@
+#include "Blend2DNet.h"
+
+//BLImage* Blend2DNet::CreateImage(int w, int h, BLFormat f) {
+//  BLImage* img = new BLImage(w, h, f);
+//  return img; 
+//}
+
+class Blend2DNet_BLImageImpl : public Blend2DNet_BLImage {
+public:
+  virtual void Init(int w, int h, BLFormat f);
+private:
+  BLImage img;
+};
+
+Blend2DNet_BLImage* Blend2DNetImpl::CreateImage(int w, int h, BLFormat f)
+{
+  Blend2DNet_BLImageImpl* instance = new Blend2DNet_BLImageImpl();
+  instance->Init(w, h, f);
+  return instance;
+}
+
+void Blend2DNet_BLImageImpl::Init(int w, int h, BLFormat f)
+{
+  blImageInitAs(&img, w, h, f);
+}
---
+++ src/Blend2DNet.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "blend2d.h"
+
+using namespace std;
+class BLImage;
+class Blend2DNet_BLImage;
+
+class Blend2DNet
+{
+public:
+  //BLImage* CreateImage(int width, int height, BLFormat format);
+  virtual Blend2DNet_BLImage* CreateImage(int w, int h, BLFormat f) = 0;
+};
+
+class Blend2DNetImpl : public Blend2DNet
+{
+public:
+  Blend2DNet_BLImage* CreateImage(int w, int h, BLFormat f);
+};
+
+Blend2DNet* CreateInstance()
+{
+  return new Blend2DNetImpl();
+};
+
+class Blend2DNet_BLImage
+{
+public:
+  virtual void Init(int w, int h, BLFormat f) = 0;
+};
+
