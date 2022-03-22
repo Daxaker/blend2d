8dc913a0c3108e1608444f099b8bb7d037bd316a
daxaker <itjustsoap@gmail.com>
2022-03-22 17:25:16 +0300

    some proxy

--- src/Blend2DNet.cpp
+++ src/Blend2DNet.cpp
@@ -1,25 +1,142 @@
 #include "Blend2DNet.h"
 
-//BLImage* Blend2DNet::CreateImage(int w, int h, BLFormat f) {
-//  BLImage* img = new BLImage(w, h, f);
-//  return img; 
-//}
+class Blend2DNet_BLImageCodecImpl : public Blend2DNet_BLImageCodec {
+public:
+  virtual BLResult FindByName(wchar_t* name) override
+  {
+    size_t len = wcslen(name);
+    char* buf = new char[len + 1];
+    size_t newLen = wcstombs(buf, name, len);
+    if (newLen > 0u)
+      buf[newLen] = '\0';
+    auto res = codec.findByName(buf);
+    delete[] buf;
+    return res;
+  }
+protected:
+  friend class Blend2DNet_BLImageImpl;
+};
+
+class Blend2DNet_BLPathImpl : public Blend2DNet_BLPath {
+public:
+  virtual BLResult MoveTo(double p0, double p1) override {
+    return path.moveTo(p0,p1);
+  }
+  virtual BLResult CubicTo(double x1, double y1, double x2, double y2, double x3, double y3) override {
+    return path.cubicTo(x1, y1, x2, y2, x3, y3);
+  }
+protected:
+  BLPath path;
+private:
+  friend class Blend2DNet_BLContextImpl;
+};
 
 class Blend2DNet_BLImageImpl : public Blend2DNet_BLImage {
 public:
-  virtual void Init(int w, int h, BLFormat f);
+  virtual BLResult ReadFromFile(wchar_t* filePath) override {
+    size_t len = wcslen(filePath);
+    char* buf = new char[len+1];
+    size_t newLen = wcstombs(buf, filePath, len);
+    if (newLen > 0u)
+      buf[newLen] = '\0';
+    BLResult res = img.readFromFile(buf);
+    delete[] buf;
+    return res;
+  }
+
+  virtual BLResult WriteToFile(wchar_t* filePath, Blend2DNet_BLImageCodec* codec) override
+  {
+    size_t len = wcslen(filePath);
+    char* buf = new char[len + 1];
+    size_t newLen = wcstombs(buf, filePath, len);
+    if (newLen > 0u)
+      buf[newLen] = '\0';
+    auto result = img.writeToFile(buf, codec->codec);
+    delete[] buf;
+    return result;
+  }
+protected:
+  virtual void Init(int w, int h, BLFormat f) override {
+    blImageInitAs(&img, w, h, f);
+  }
 private:
-  BLImage img;
+
+  friend class Blend2DNetImpl;
 };
 
-Blend2DNet_BLImage* Blend2DNetImpl::CreateImage(int w, int h, BLFormat f)
-{
-  Blend2DNet_BLImageImpl* instance = new Blend2DNet_BLImageImpl();
-  instance->Init(w, h, f);
-  return instance;
-}
+class Blend2DNet_BLContextImpl : public Blend2DNet_BLContext {
+public:
+  virtual BLResult SetCompOp(BLCompOp compOp) override
+  {
+    return context.setCompOp(compOp);
+  }
+  virtual BLResult FillAll() override
+  {
+    return context.fillAll();
+  }
+  virtual BLResult SetFillStyle(BLRgba32& rgba) override
+  {
+    return context.setFillStyle(rgba);
+  }
+  virtual BLResult FillPath(Blend2DNet_BLPath* path) override
+  {
+    auto pathImpl = dynamic_cast<Blend2DNet_BLPathImpl*>(path);
+    return context.fillPath(pathImpl->path);
+  }
+  virtual BLResult End() override {
+    return context.end();
+  }
+protected:
+  void Init(BLImage& img) {
+    blContextInitAs(&context, &img, nullptr);
+  }
+  BLContext context;
 
-void Blend2DNet_BLImageImpl::Init(int w, int h, BLFormat f)
+  friend class Blend2DNetImpl;
+};
+
+class Blend2DNetImpl : public Blend2DNet
 {
-  blImageInitAs(&img, w, h, f);
-}
+public:
+  virtual Blend2DNet_BLImage* Blend2DNetImpl::CreateImage(int w, int h, BLFormat f)
+  {
+    Blend2DNet_BLImageImpl* instance = new Blend2DNet_BLImageImpl();
+    instance->Init(w, h, f);
+    return instance;
+  }
+
+  virtual Blend2DNet_BLImage* CreateImage() override
+  {
+    Blend2DNet_BLImageImpl* instance = new Blend2DNet_BLImageImpl();
+    return instance;
+  }
+
+  virtual Blend2DNet_BLContext* Blend2DNetImpl::CreateContext(Blend2DNet_BLImage* img) override
+  {
+    Blend2DNet_BLContextImpl* instance = new Blend2DNet_BLContextImpl();
+    instance->Init(img->img);
+    return instance;
+  }
+
+  virtual Blend2DNet_BLPath* CreatePath() override
+  {
+    Blend2DNet_BLPathImpl* path = new Blend2DNet_BLPathImpl();
+    return path;
+  };
+
+  virtual Blend2DNet_BLImageCodec* CreateImageCodec() override
+  {
+    Blend2DNet_BLImageCodecImpl* codec= new Blend2DNet_BLImageCodecImpl();
+    return codec;
+  }
+};
+
+Blend2DNet* CreateInstance()
+{
+  return new Blend2DNetImpl();
+};
+
+int TestInterop()
+{
+  return 42;
+};
--- src/Blend2DNet.h
+++ src/Blend2DNet.h
@@ -2,30 +2,59 @@
 #include "blend2d.h"
 
 using namespace std;
-class BLImage;
+class Blend2DNetImpl;
 class Blend2DNet_BLImage;
+class Blend2DNet_BLContext;
+class Blend2DNet_BLPath;
+class Blend2DNet_BLImageCodec;
 
 class Blend2DNet
 {
 public:
-  //BLImage* CreateImage(int width, int height, BLFormat format);
   virtual Blend2DNet_BLImage* CreateImage(int w, int h, BLFormat f) = 0;
+  virtual Blend2DNet_BLImage* CreateImage() = 0;
+  virtual Blend2DNet_BLContext* CreateContext(Blend2DNet_BLImage* img) = 0;
+  virtual Blend2DNet_BLPath* CreatePath() = 0;
+  virtual Blend2DNet_BLImageCodec* CreateImageCodec() = 0;
 };
 
-class Blend2DNetImpl : public Blend2DNet
+class Blend2DNet_BLImageCodec {
+public:
+  BLImageCodec codec;
+  virtual BLResult FindByName(wchar_t* name) = 0;
+};
+
+class Blend2DNet_BLPath
 {
 public:
-  Blend2DNet_BLImage* CreateImage(int w, int h, BLFormat f);
-};
-
-Blend2DNet* CreateInstance()
-{
-  return new Blend2DNetImpl();
+  virtual BLResult MoveTo(double, double) = 0;
+  virtual BLResult CubicTo(double x1, double y1, double x2, double y2, double x3, double y3) = 0;
 };
 
 class Blend2DNet_BLImage
 {
 public:
+  virtual BLResult ReadFromFile(wchar_t*) = 0;
+  virtual BLResult WriteToFile(wchar_t*, Blend2DNet_BLImageCodec*) = 0;
+  BLImage img;
+protected:
   virtual void Init(int w, int h, BLFormat f) = 0;
+private:
+  friend class Blend2DNet;
 };
+
+class Blend2DNet_BLContext {
+public:
+  virtual BLResult SetCompOp(BLCompOp) = 0;
+  virtual BLResult FillAll() = 0;
+  virtual BLResult SetFillStyle(BLRgba32& rgba) = 0;
+  virtual BLResult FillPath(Blend2DNet_BLPath* path) = 0;
+  virtual BLResult End() = 0;
+};
+
+extern "C"
+{
+  __declspec(dllexport) Blend2DNet* CreateInstance();
+  __declspec(dllexport) int TestInterop();  
+}
 
